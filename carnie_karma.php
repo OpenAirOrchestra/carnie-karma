4a085cc38dd0b38bc20e5faf337e17f85724eba7
Darryl Fuller <darrylf@apple.com>
2012-11-27 21:14:51 -0800

    Stubbing in initial boilerplate.

---
+++ carnie_karma.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Carnie Karma 
+Plugin URI: http://www.thecarnivalband.com
+Description: A plugin to calculate and display participation Karma for The Carnival Band
+Version: 0.1
+Author: DarrylF
+Author URI: http://www.thecarnivalband.com
+License: GPL2
+*/
+?>
+<?php
+/*  Copyright 2012  DarrylF (email : oaowebmonkey@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+/*
+ * Main class for Carnie Karma Handles activation, hooks, etc.
+ */
+class carnieKarma {
+
+
+};
+
+// instantiate class
+$CARNIEKARMA = new carnieKarma;
+
+// add_action('admin_menu', array($CARNIEKARMA, 'create_admin_menu'));
+
+register_activation_hook(__FILE__, array($CARNIEKARMA, 'activate'));
+
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Workshop Attendance ===
+Contributors: darrylf
+Donate link: http://www.thecarnivalband.com/
+Tags: gigs, particpation
+Requires at least: 3.2
+Tested up to: 3.2.1
+Stable tag: 0.1
+
+A plugin to calculate and display participation Karma for The Carnival Band
+
+== Description ==
+
+Calculate a Karma rating based on participation in Carnival Band
+Workshops and Gigs.  Karma is based on participation, with a
+decay factor with a half life of about a year, so recent particpation
+is what matters most.  But what have you done for me lately?
+
+This plugin depends upon the carnig-gigs plugin and the
+workshop-attendance plugin
+
+== Installation ==
+
+1. Install and activate the carnie-gigs wordpress plugin.
+2. Install and activate the workshop-attendance wordpress plugin.
+3. Upload this plugin to the `/wp-content/plugins/` directory
+4. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Why did you create this plugin =
+
+Because the Open Air Orchestra Board asked me to.
+
+== Changelog ==
+
+= 0.1 =
+* Initial Version.
+
