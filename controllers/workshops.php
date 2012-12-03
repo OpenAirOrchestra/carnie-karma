f7c18a6da1dee00ddd515a7ce0108262c2e606fd
Darryl Fuller <darrylf@apple.com>
2012-12-02 20:46:36 -0800

    Query to get workshops

--- controllers/workshops.php
+++ controllers/workshops.php
@@ -9,7 +9,32 @@ class carnieKarmaWorkshopsController {
 	 * Does a detailed report of workshop karma for a user.
 	 */
 	function report($user_id) {
-                echo "<h2>DFDF: Workshop Participation Karma For User " . $user_id . "</h2>";
+
+		global $wpdb;
+
+		$workshop_karma_view_name = $wpdb->prefix . "workshop_karma";
+
+		// Get all the data (paged view will come later)
+
+		$sql = $wpdb->prepare(
+			"
+			SELECT *
+			  FROM  $workshop_karma_view_name
+			  WHERE  user_id = %d
+			  ORDER BY date DESC
+			",
+			$user_id
+			);
+
+		$results = $wpdb->get_results($sql, ARRAY_A);
+
+                echo "<h2>TODO: Workshop Participation Karma For User " . $user_id . "</h2>";
+
+		echo "<ul>";
+		foreach ($results as $row) {
+			echo "<li> workshop </li>";
+		}
+		echo "</ul>";
 	}
 }
 ?>
