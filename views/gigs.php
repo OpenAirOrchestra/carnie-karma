619337e70514ee39c781f1b7c129812d3ac21fd3
Darryl Fuller <darrylf@apple.com>
2012-12-05 20:43:27 -0800

    First light of day on verified gig participation detail view.

---
+++ views/gigs.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Renders a gigs karma report for a user.
+ */       
+class carnieKarmaGigsView {
+
+        /*
+         * render table header/footer
+         */
+        function render_table_header_footer() {
+?>
+                <tr>
+			<th>Date</th>
+			<th>Title</th>
+			<th>Karma</th>
+                </tr>
+<?php
+	}
+
+ 
+	/*
+ 	 * Renders a gig karma report for a user.
+	 */
+	function render($user_id, $gigs, $count, $paged, $limit) {
+
+                $siteurl = get_bloginfo('siteurl');
+		$edit_url = $siteurl . '/wp-admin/user-edit.php?user_id=' . $user_id;
+                $user_info = get_userdata($user_id);
+
+		// Title
+		print "<h2>Verified Gig Participation Karma for "; 
+		if ($user_info->first_name || $user_info->last_name) {
+			echo $user_info->first_name . ' ' . $user_info->last_name;
+			echo ' (<a href="' . $edit_url . '">' .
+				$user_info->user_login . 
+				'</a>)';
+		} else {
+			echo $user->display_name;
+			echo ' (<a href="' . $edit_url . '">' .
+				$user_info->user_login . 
+				'</a>)';
+		}
+		print "</h2>";
+
+                $karma_detail_nonce = wp_create_nonce('karma_detail_nonce');
+
+		$tableView = new carnieKarmaTableView;
+		$tableView->render_table_nav("top", "gig", $karma_detail_nonce, $user_id, $count, $paged, $limit);
+
+		print "<table>";
+		print "<thead>";
+		$this->render_table_header_footer();
+		print "</thead>";
+		print "<tfoot>";
+		$this->render_table_header_footer();
+		print "</tfoot>";
+		print "<tbody>";
+		foreach ($gigs as $gig) {
+
+			$gig_url = get_permalink($gig['gigid']);
+
+			print "<tr>";
+			echo "<td>" .  $gig['date'] . " </td>";
+			echo "<td>";
+			echo '<a href="' . $gig_url . '">';
+			echo $gig['title'];
+			echo "</a>";
+			echo "</td>";
+			echo "<td>" .  $gig['karma'] . " </td>";
+			print "</tr>";
+		}
+		print "</tbody>";
+		print "</table>";
+
+
+		$tableView->render_table_nav("top", "gig", $karma_detail_nonce, $user_id, $count, $paged, $limit);
+
+	}
+}
+?>
