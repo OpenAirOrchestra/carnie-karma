fa71f9ffc4bd5250a31ad9230c2f2f5683c3d6e0
Darryl Fuller <darrylf@apple.com>
2012-12-05 20:03:50 -0800

    Create verified gig attendance view

--- carnie-karma.php
+++ carnie-karma.php
@@ -91,6 +91,53 @@ class carnieKarma {
 			*/
 			$wpdb->query($sql);
 			
+
+			// Create verified gig attendance view
+                        $gig_karma_view_name = $wpdb->prefix . "gig_karma";
+                        $posts_name = $wpdb->prefix . "posts";
+                        $postmeta_name = $wpdb->prefix . "postmeta";
+                        $gig_attendance_name = $wpdb->prefix . "gig_attendance";
+
+			/*
+			CREATE VIEW 
+				wp_gig_karma
+			AS
+			SELECT 
+				wp_gig_attendance.gigid AS gigid,
+				wp_posts.post_title AS title,
+				wp_gig_attendance.user_id AS userid,
+				wp_postmeta.meta_value AS date,
+				POW(0.998 , ( DATEDIFF( CURRENT_DATE( ) , wp_postmeta.meta_value) ) ) AS karma 
+			FROM 
+				wp_gig_attendance, wp_postmeta, wp_posts
+			WHERE 
+				wp_gig_attendance.gigid = wp_postmeta.post_id
+				AND wp_gig_attendance.gigid = wp_posts.ID
+				AND wp_postmeta.meta_key =  "cbg_date"
+				AND ( wp_gig_attendance.deleted IS NULL OR  wp_gig_attendance.deleted = 0 )
+			*/
+
+			$sql = "
+				CREATE VIEW 
+					$gig_karma_view_name
+				AS
+				SELECT 
+					$gig_attendance_name.gigid AS gigid,
+					$posts_name.post_title AS title,
+					$gig_attendance_name.user_id AS userid,
+					$postmeta_name.meta_value AS date,
+					POW(0.998 , ( DATEDIFF( CURRENT_DATE( ) , $postmeta_name.meta_value) ) ) AS karma 
+				FROM 
+					$gig_attendance_name, $postmeta_name, $posts_name
+				WHERE 
+					$gig_attendance_name.gigid = $postmeta_name.post_id
+					AND $gig_attendance_name.gigid = $posts_name.ID
+					AND $postmeta_name.meta_key =  \"cbg_date\"
+					AND ( $gig_attendance_name.deleted IS NULL OR  $gig_attendance_name.deleted = 0 )
+			";
+
+			$wpdb->query($sql);
+
 			add_option("carniekarma_db_version", CARNIE_KARMA_DB_VERSION);
 		}
 
