98ec26b35413e5d22181b1f568add7d4bba76b73
Darryl Fuller <darrylf@apple.com>
2012-12-12 10:08:10 -0800

    Stub in some table methods.

--- views/load.php
+++ views/load.php
@@ -5,10 +5,71 @@
  */       
 class carnieKarmaLoadView {
 
+
+        /*
+         * render table navigation top or bottom
+         */
+        function render_table_nav( $position, $all_count, $filtered_count, $limit, $paged ) {
+		// TODO
+	}
+ 
+        /*
+         * render table header/footer
+         */
+        function render_table_header_footer($orderBy, $order) {
+		// TODO
+        }
+
+        /*
+         * render a row in workshop table
+         */
+        function render_row( $row, $karma_ledger_delete_nonce, $karma_ledger_nonce ) {
+		// TODO
+?>
+		<tr><td>ROW</td></tr>
+<?php
+	}
+
+        /*
+         * render karmic load ledger table body
+         */
+        function render_table_body( $rows ) {
+                echo '          <tbody id="the-list">';
+
+                $karma_ledger_delete_nonce = wp_create_nonce('karma_ledger_delete_nonce');
+                $karma_ledger_nonce = wp_create_nonce('karma_ledger_nonce');
+
+                foreach ($rows as $row)
+                {
+                        $this->render_row($row, $karma_ledger_delete_nonce, $karma_ledger_nonce);
+                }
+                echo '          </tbody>';
+        }
+
+
+	/*
+	 * Render karmic load as a table/ledger
+ 	 */
 	function render_table( $rows, $orderBy, $order, $all_count, $filtered_count, $limit, $paged ) {
 
+		$this->render_table_nav( "top", $all_count, $filtered_count, $limit, $paged );
+?>
 
-		echo "<p>TODO: render table</p>";
+                        <table class="wp-list-table widefat fixt posts" cellspacing="0">
+                                <thead>
+                                        <?php $this->render_table_header_footer($orderBy, $order); ?>
+                                </thead>
+                                <tfoot>
+                                        <?php $this->render_table_header_footer($orderBy, $order); ?>
+                                </tfoot>
+                                <?php $this->render_table_body($rows); ?>
+
+                        </table>
+
+
+<?php
+
+		$this->render_table_nav( "bottom", $all_count, $filtered_count, $limit, $paged );
 	}
 
 }
