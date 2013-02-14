f5235a0f8e0d569d74382fd592808ac9e60563ff
Darryl Fuller <darrylf@apple.com>
2013-02-13 21:38:35 -0800

    First csv output

--- balance_csv.php
+++ balance_csv.php
@@ -10,35 +10,56 @@ global $wpdb;
 global $current_user;
 
 $Filename = "KarmaBalances.csv";
-header("Content-Type: text/csv");
-header("Content-Disposition: attachment; filename=$Filename");
+// header("Content-Type: text/csv");
+// header("Content-Disposition: attachment; filename=$Filename");
 
 // Verify nonce.
 if ( wp_verify_nonce($_POST['karma-balance-csv-verify-key'], 'karma-balance-csv-verify-key') ) {
 
-
 	if (current_user_can('read_private_posts')) {
 
-	/*
-        foreach ($results as $row) {
-                $separator = "";
-                foreach ($row as $fieldname=>$field) {
-			echo $separator;
+		$users = get_users('orderby=nicename');
+		foreach ($users as $user) {
+			if ($user->ID != 1) {
+			
+				$user_info = get_userdata($user->ID);
 
-			// handle NULL
-			if ($field != NULL) {
-				// escape " character in field
-				$field = str_replace("\"", "\"\"", $field);
-				// strip newlines in field
-				$field = str_replace(array('\n', '\r'), " ", $field);
+				// user id
+				echo '$user->ID' . ',';
 
+				// user nicename
+				$field = $user->nicename;
+				if ($field != NULL) {
+					$field = str_replace("\"", "\"\"", $field);
+					$field = str_replace(array('\n', '\r'), " ", $field);
+				}
+				echo "\"" . stripslashes($field) . "\"";
+				echo ",";
+
+				// user firstname
+				$field = $user_info->first_name;
+				if ($field != NULL) {
+					$field = str_replace("\"", "\"\"", $field);
+					$field = str_replace(array('\n', '\r'), " ", $field);
+				}
+				echo "\"" . stripslashes($field) . "\"";
+				echo ",";
+
+				// user lastname
+				$field = $user_info->first_name;
+				if ($field != NULL) {
+					$field = str_replace("\"", "\"\"", $field);
+					$field = str_replace(array('\n', '\r'), " ", $field);
+				}
+				echo "\"" . stripslashes($field) . "\"";
+				echo ",";
+
+				// balance
+
+                		echo "\n";
 			}
-			echo "\"" . stripslashes($field) . "\"";
-			$separator = ",";
-                }
-                echo "\n";
-        }
-	*/
+		}
+
 	} else {
 		echo '"security failure", "permissions"';
 	}
