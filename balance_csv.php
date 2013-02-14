28720ed3b0fecefac6f66335f9ef939bce483125
Darryl Fuller <darrylf@apple.com>
2013-02-13 21:54:03 -0800

    Pull down those totals

--- balance_csv.php
+++ balance_csv.php
@@ -5,6 +5,7 @@
  */
 require_once 'ajaxSetup.php';
 require_once 'version.php';
+require_once 'model/multipliers.php';
 
 global $wpdb;
 global $current_user;
@@ -55,7 +56,49 @@ if ( wp_verify_nonce($_POST['karma-balance-csv-verify-key'], 'karma-balance-csv-
 				echo ",";
 
 				// balance
+				$workshop_karma_view_name = $wpdb->prefix . "workshop_karma";
+				$gig_karma_view_name = $wpdb->prefix . "gig_karma";
+				$karma_load_view_name = $wpdb->prefix . "karmic_load";
 
+				// Get summary data For workshops
+				$sql = $wpdb->prepare(
+					"
+					SELECT ( %d * SUM(  karma ) ) AS workshop_karma
+					  FROM  $workshop_karma_view_name
+					  WHERE  user_id = %d
+					",
+					CARNIE_KARMA_WORKSHOP_MULTIPLIER,
+					$user->ID
+					);
+
+				$workshop_row = $wpdb->get_row($sql, ARRAY_A);
+
+				// Get summary data For gigs
+				$sql = $wpdb->prepare(
+					"
+					SELECT (%d * SUM(  karma )) AS gig_karma
+					  FROM  $gig_karma_view_name
+					  WHERE  userid = %d
+					",
+					CARNIE_KARMA_GIG_MULTIPLIER,
+					$user->ID
+					);
+				$gig_row = $wpdb->get_row($sql, ARRAY_A);
+
+				// Get summary data For karmic load
+				$sql = $wpdb->prepare(
+					"
+					SELECT (%d * SUM(  karma )) AS karmic_load
+					  FROM  $karma_load_view_name
+					  WHERE  userid = %d
+					",
+					CARNIE_KARMA_LOAD_MULTIPLIER,
+					$user->ID
+					);
+				$load_row = $wpdb->get_row($sql, ARRAY_A);
+
+				$total = $gig_row['gig_karma'] + $workshop_row['workshop_karma'] - $load_row['karmic_load'];
+				echo $total;
                 		echo "\n";
 			}
 		}
